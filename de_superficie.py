58beb4b593549f401c253ec80e89a1db7224187f
Cris Miranda <cristianmirandapadilla003@gmail.com>
2024-09-21 16:53:50 -0600

    Implementacion

--- de_superficie.py
+++ de_superficie.py
@@ -5,3 +5,10 @@ from numpy import arange, sqrt, sin, meshgrid
 fig = figure()
 ax = fig.gca(projection = '3d')
 
+X = arange(-5, 5, 0.25)
+Y = arange(-5, 5, 0.25)
+X, Y = meshgrid(X, Y)
+Z = sin( sqrt(X**2 + Y**2))
+A = ax.plot_surface(X, Y, Z, rstride = 1, cstride = 1, cmap = cm.coolwarm)
+title('Grafica de Superficie')
+show() 
