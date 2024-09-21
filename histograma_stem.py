3363358ed4aa94581bf77eb3e0936a08cd3dd893
Cris Miranda <cristianmirandapadilla003@gmail.com>
2024-09-21 15:56:00 -0600

    Codigo Histograma

---
+++ histograma_stem.py
@@ -0,0 +1,6 @@
+from matplotlib.pyplot import title, hist, show
+from numpy.random import normal
+numeros_gaussianos = normal(size = 1000)
+hist(numeros_gussianos)
+title("Grafica de Historgrama")
+show()
