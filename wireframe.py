0e2aa1e09d6f0f2323d1f03383eb39715664cf3d
Cris Miranda <cristianmirandapadilla003@gmail.com>
2024-09-21 17:16:19 -0600

    Finalizacion y Documentacion

--- wireframe.py
+++ wireframe.py
@@ -4,3 +4,11 @@ from numpy import arange, sqrt, sin, meshgrid
 
 fig = figure()
 ax = fig.gca(projection = '3d')
+
+X = arange(-5, 5, 0.50)
+Y = arange(-5, 5, 0.50)
+X, Y = meshgrid(X, Y)
+Z = sin( sqrt(X**2 + Y**2))
+A = ax.plot_wireframe(X, Y, Z)
+title('Grafica de Superficie de Alambre')
+show() 
