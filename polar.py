7a9cb3fbc614321d775847e1272221b2daae302e
Cris Miranda <cristianmirandapadilla003@gmail.com>
2024-09-21 14:56:50 -0600

    Correcciones

--- polar.py
+++ polar.py
@@ -1,12 +1,12 @@
 from numpy import pi, arange, linspace
 from matplotlib.pyplot import subplot, plot, show, grid, title
 
-tetat=range(0, 3, 0.01) #Generamos los valres de cero
-r= 2*pi * teta #Se calcula r para cada valor de cero
-subplot(111, polar = true) #Creamos la subgráfica polar
+teta=arange(0, 3, 0.01) #Generamos los valres de cero
+r= 2*pi * teta #Se calcular para cada valor de cero
+subplot(111, polar = True) #Creamos la subgráfica polar
 plot (r, teta, color = 'r', linewidth = 3) #Creamos la gráfica
-grid(True) # Se añade la reticula
-title("Grfica polar de r = 2$\pi\\teta$")
+grid(True) # Se añade la linea
+title("Grafica polar ")
 show()
  # La gráfica polar se obtine usando la instrucción plot pero usando componentes (radio vector)
  # y (angulo argumento), cualquiera de los dos o los dos pueden variar en un angulo determinado.
