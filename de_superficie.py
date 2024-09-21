c61653b0ca5b341add7390af2e06562d3094b6af
Cris Miranda <cristianmirandapadilla003@gmail.com>
2024-09-21 16:53:21 -0600

    Inicio

---
+++ de_superficie.py
@@ -0,0 +1,7 @@
+from matplotlib import cm
+from matplotlib.pyplot import plot, figure, show, title
+from numpy import arange, sqrt, sin, meshgrid
+
+fig = figure()
+ax = fig.gca(projection = '3d')
+
