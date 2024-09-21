55a44f1deef5488a0efa57e5471bfa946199d8f2
Cris Miranda <cristianmirandapadilla003@gmail.com>
2024-09-21 14:36:23 -0600

    Documentacion completa

--- polar.py
+++ polar.py
@@ -1,7 +1,7 @@
 from numpy import pi, arange, linspace
 from matplotlib.pyplot import subplot, plot, show, grid, title
 
-tetat=range(0, 3, 0.01) #Generamos los valroes de cero
+tetat=range(0, 3, 0.01) #Generamos los valres de cero
 r= 2*pi * teta #Se calcula r para cada valor de cero
 subplot(111, polar = true) #Creamos la subgráfica polar
 plot (r, teta, color = 'r', linewidth = 3) #Creamos la gráfica
