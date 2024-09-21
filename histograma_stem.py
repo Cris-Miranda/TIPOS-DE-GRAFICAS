0ffce8ca6a1a2f4824a5f47033c4418f05070dc0
Cris Miranda <cristianmirandapadilla003@gmail.com>
2024-09-21 16:03:44 -0600

    Codigo Stem

--- histograma_stem.py
+++ histograma_stem.py
@@ -1,6 +1,12 @@
 from matplotlib.pyplot import title, hist, show
 from numpy.random import normal
 numeros_gaussianos = normal(size = 1000)
-hist(numeros_gussianos)
+hist(numeros_gaussianos)
 title("Grafica de Historgrama")
+show()
+
+# Codigo grafica stem
+from matplotlib.pyplot import stem, show
+from numpy import pi, arange 
+stem( arange(-pi, pi))
 show()
